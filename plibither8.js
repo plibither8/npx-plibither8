7194b25da2764f07f6bbfe1f579a6b29a8267804
Mihir Chaturvedi <plibither8@gmail.com>
2019-07-28 00:09:11 +0530

    Add basic script

--- plibither8.js
+++ plibither8.js
@@ -1,3 +1,29 @@
 #!/usr/bin/env node
 
-console.log('hello')
+const sentences = [
+	"        _  _  _      _  _    _                  ___  ",
+	" _ __  | |(_)| |__  (_)| |_ | |__    ___  _ __ ( _ ) ",
+	"| '_ \\ | || || '_ \\ | || __|| '_ \\  / _ \\| '__|/ _ \\ ",
+	"| |_) || || || |_) || || |_ | | | ||  __/| |  | (_) |",
+	"| .__/ |_||_||_.__/ |_| \\__||_| |_| \\___||_|   \\___/ ",
+	"|_|                                                  ",
+	"",
+	"Hey there! Thanks for trying this out. :)",
+	"",
+	"I will be updating this soon to including more stuff.",
+	"You must have already realised that",
+	"...hopefully",
+	"",
+	"See you!",
+	"Good times, good times",
+	"",
+	" — Mihir Chaturvedi (plibither8)",
+	"",
+	"   mihir.ch             | twitter.com/plibither8",
+	"   plibither8@gmail.com | github.com/plibither8",
+	"",
+];
+
+for (const sentence of sentences) {
+	console.log(sentence);
+}
